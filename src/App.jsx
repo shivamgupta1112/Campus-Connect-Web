f8d5c2026ed0b36c802d852f3f5f8e77c9d5f13a
Shivam Gupta <shivamgupta11122004@gmail.com>
2026-01-11 12:01:09 +0530

    + router & cosmetics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="bg-white">
-      <Header />
-      <main>
-        <Hero />
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<div className="bg-white">
+        <Header />
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+
+      <Route path="/about" element={<div className="bg-white">
+        <Header />
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+      <Route path="/contact" element={<div className="bg-white">
+        <Header />
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+      <Route path="/get-started" element={<div className="bg-white">
+        <Header />
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+      <Route path="/login" element={<div className="bg-white">
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+      <Route path="/register" element={<div className="bg-white">
+        <main>
+          <Hero />
+        </main>
+      </div>} />
+
+    </Routes>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Header = () => {
+    const navigate = useNavigate();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
 
@@ -18,15 +19,15 @@ const Header = () => {
         <>
             <header
                 className={`fixed z-50 transition-all duration-500 ease-in-out border-b border-white/10 ${isScrolled
-                    ? "top-4 left-1/2 -translate-x-1/2 w-[95%] sm:w-[90%] md:max-w-5xl rounded-lg bg-white/60 backdrop-blur-xl shadow-2xl border-white/20"
-                    : "top-0 left-0 w-full bg-white/60 backdrop-blur-md border-gray-100"
+                    ? "top-4 left-1/2 -translate-x-1/2 w-[95%] sm:w-[90%] md:max-w-5xl rounded-2xl bg-slate-900/90 backdrop-blur-xl shadow-2xl border-white/10"
+                    : "top-0 left-0 w-full bg-transparent border-transparent"
                     }`}
             >
                 <div className={`mx-auto px-4 sm:px-6 lg:px-8 transition-all duration-500 ${isScrolled ? "h-16" : "h-20"}`}>
                     <div className="flex justify-between items-center h-full">
                         {/* Logo Section */}
                         <div className="flex-shrink-0 flex items-center cursor-pointer gap-2">
-                            <img src="/logo.png" alt="Campus Connect Logo" className={`object-contain transition-all duration-500 ${isScrolled ? "h-32" : "h-40"}`} />
+                            <img src="/logo.png" onClick={() => navigate('/')} alt="Campus Connect Logo" className={`object-contain transition-all duration-500 brightness-0 invert ${isScrolled ? "h-32" : "h-40"}`} />
                         </div>
 
                         {/* Desktop Navigation */}
@@ -35,7 +36,8 @@ const Header = () => {
                                 <Link
                                     key={item}
                                     to={item === 'Home' ? '/' : `/${item.toLowerCase()}`}
-                                    className="px-4 py-2 text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50/50 transition-all duration-200"
+                                    onClick={() => navigate(item === 'Home' ? '/' : `/${item.toLowerCase()}`)}
+                                    className="px-4 py-2 text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50/50 transition-all duration-200"
                                 >
                                     {item}
                                 </Link>
@@ -59,7 +61,7 @@ const Header = () => {
 
                         {/* Mobile Menu Button */}
                         <button
-                            className="md:hidden text-gray-600 p-2 hover:bg-gray-100 rounded-full transition-colors"
+                            className="md:hidden text-white p-2 hover:bg-white/10 rounded-full transition-colors"
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
                         >
                             {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
